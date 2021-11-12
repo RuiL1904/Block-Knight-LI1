e52b4751663fe438c6daf2a4a5f30097efb2ee5e
RuiL1904 <rui.lilokas2003@hotmail.com>
2021-11-12 09:40:27 +0000

    Add Tarefa 1.1

--- src/Tarefa1_2021li1g033.hs
+++ src/Tarefa1_2021li1g033.hs
@@ -10,6 +10,18 @@ module Tarefa1_2021li1g033 where
 
 import LI12122
 
+-- | Verifica se o 'Mapa' é válido.
 validaPotencialMapa :: [(Peca, Coordenadas)] -> Bool
-validaPotencialMapa pecas = undefined
+validaPotencialMapa [] = False
+validaPotencialMapa l@(h:t) = pecaUnica l
+
+-- | Verifica se as peças têm todas coordenadas diferentes.
+pecaUnica :: [(Peca, Coordenadas)] -> Bool
+pecaUnica [] = True
+pecaUnica ((_,s):t) = pertenceCauda s t && pecaUnica t
+
+-- | Auxiliar de pecaUnica - Verifica se uma coordenada pertence à cauda (tail).
+pertenceCauda :: Coordenadas -> [(Peca,Coordenadas)] -> Bool
+pertenceCauda _ [] = True
+pertenceCauda c ((_,s):t) = c /= s && pertenceCauda c t
 

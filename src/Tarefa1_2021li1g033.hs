920a66f9bccf54efc55439576f97255bbaabfce4
RuiL1904 <rui.lilokas2003@hotmail.com>
2021-11-14 15:30:03 +0000

    Add one more verification to Tarefa 1.1

--- src/Tarefa1_2021li1g033.hs
+++ src/Tarefa1_2021li1g033.hs
@@ -13,15 +13,19 @@ import LI12122
 -- | Verifica se o 'Mapa' é válido.
 validaPotencialMapa :: [(Peca, Coordenadas)] -> Bool
 validaPotencialMapa [] = False
-validaPotencialMapa l@(h:t) = pecaUnica l
+validaPotencialMapa l = coordenadaDiferente l
 
 -- | Verifica se as peças têm todas coordenadas diferentes.
-pecaUnica :: [(Peca, Coordenadas)] -> Bool
-pecaUnica [] = True
-pecaUnica ((_,s):t) = pertenceCauda s t && pecaUnica t
+coordenadaDiferente :: [(Peca, Coordenadas)] -> Bool
+coordenadaDiferente [] = True
+coordenadaDiferente l@((_,s):t) = pertenceCauda s t && coordenadaDiferente t && coordenadaPositiva l
 
--- | Auxiliar de pecaUnica - Verifica se uma coordenada pertence à cauda (tail).
-pertenceCauda :: Coordenadas -> [(Peca,Coordenadas)] -> Bool
+-- | Auxiliar de coordenadaDiferente - Verifica se uma coordenada pertence à cauda (tail).
+pertenceCauda :: Coordenadas -> [(Peca, Coordenadas)] -> Bool
 pertenceCauda _ [] = True
 pertenceCauda c ((_,s):t) = c /= s && pertenceCauda c t
 
+-- | Auxiliar de coordenadaDiferente - Verifica se as coordenadas são positivas.
+coordenadaPositiva :: [(Peca, Coordenadas)] -> Bool
+coordenadaPositiva [] = True
+coordenadaPositiva ((_,(x,y)):t) = x > 0 && y > 0 && coordenadaPositiva t

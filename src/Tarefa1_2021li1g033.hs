ef0f9c4980d1b17a6494c251f283b7d26d896f65
RuiL1904 <rui.lilokas2003@hotmail.com>
2021-11-16 15:19:36 +0000

    Add Tarefa 1.3

--- src/Tarefa1_2021li1g033.hs
+++ src/Tarefa1_2021li1g033.hs
@@ -10,35 +10,58 @@ module Tarefa1_2021li1g033 where
 
 import LI12122
 
--- | Verifica se o 'Mapa' é válido.
+-- | Verifica se o mapa é válido.
 validaPotencialMapa :: [(Peca, Coordenadas)] -> Bool
-validaPotencialMapa [] = False
-validaPotencialMapa l = coordenadaDiferente l && portaUnica l
+validaPotencialMapa l = coordenadaDiferente l  
+                        && coordenadaPositiva l
+                        && portaUnica l 
+                        && caixasAFlutuar l 
+                        && peloMenosUmVazio l
+
+-- TAREFA 1.1
 
 -- | Verifica se as peças têm todas coordenadas diferentes.
 coordenadaDiferente :: [(Peca, Coordenadas)] -> Bool
 coordenadaDiferente [] = True
-coordenadaDiferente l@((_,s):t) = pertenceCauda s t && coordenadaDiferente t && coordenadaPositiva l
+coordenadaDiferente l@((_,s):t) = pertenceCauda s t && coordenadaDiferente t
 
 -- | Auxiliar de coordenadaDiferente - Verifica se uma coordenada pertence à cauda (tail).
 pertenceCauda :: Coordenadas -> [(Peca, Coordenadas)] -> Bool
 pertenceCauda _ [] = True
 pertenceCauda c ((_,s):t) = c /= s && pertenceCauda c t
 
--- | Auxiliar de coordenadaDiferente - Verifica se as coordenadas são positivas.
+-- | Verifica se as coordenadas são todas positivas, visto que o mapa começa nas coordenadas (0,0).
 coordenadaPositiva :: [(Peca, Coordenadas)] -> Bool
 coordenadaPositiva [] = True
-coordenadaPositiva ((_,(x,y)):t) = x > 0 && y > 0 && coordenadaPositiva t
+coordenadaPositiva ((_,(x,y)):t) = x >= 0 && y >= 0 && coordenadaPositiva t
 
--- | Verifica se existe uma e uma só 'Porta'.
+-- TAREFA 1.2
+
+-- | Verifica se existe uma e uma só porta.
 portaUnica :: [(Peca, Coordenadas)] -> Bool
-portaUnica [] = True
 portaUnica l = contaPortas l == 1
 
--- | Auxiliar de portaUnica - Conta quantas portas existem no 'Mapa'.
+-- | Auxiliar de portaUnica - Conta quantas portas existem no mapa.
 contaPortas :: [(Peca, Coordenadas)] -> Int
 contaPortas [] = 0
 contaPortas ((f,_):t) =
     case f of
         Porta -> 1 + contaPortas t
         _ -> contaPortas t
+
+-- TAREFA 1.3
+
+-- | Verifica se não existem caixas a flutuar.
+caixasAFlutuar :: [(Peca, Coordenadas)] -> Bool
+caixasAFlutuar [] = True
+caixasAFlutuar l@((f,(x,y)):t) =
+    case f of
+        Caixa -> encontraPeca (x, y + 1) l && caixasAFlutuar t
+        _ -> caixasAFlutuar t
+
+-- | Auxiliar de caixasAFlutuar - Verifica se existe alguma peça na posição dada (neste caso: y + 1).
+encontraPeca :: Coordenadas -> [(Peca, Coordenadas)] -> Bool
+encontraPeca _ [] = False
+encontraPeca c ((f,c'@(x',y')):t)
+    | c == c' && (f == Caixa || f == Bloco) = True -- ^ Verifica também se a peça em questão é uma caixa ou um bloco.
+    | otherwise = encontraPeca c t

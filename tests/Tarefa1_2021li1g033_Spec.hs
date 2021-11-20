b74f58cf58dd0e33440e366ceb2f03ead278b142
Rui Martins <a100643@alunos.uminho.pt>
2021-11-20 23:28:52 +0000

    Add some tests

--- tests/Tarefa1_2021li1g033_Spec.hs
+++ tests/Tarefa1_2021li1g033_Spec.hs
@@ -8,7 +8,9 @@ import Fixtures
 -- Tarefa 1
 testsT1 =
   test
-    [ "Tarefa 1 - Teste Valida Mapa m1r" ~: validaPotencialMapa m1 ~=? True
+    [ "Tarefa 1 - Teste Valida Mapa m1" ~: validaPotencialMapa m1 ~=? True
     , "Tarefa 1 - Teste Valida Mapa vazio" ~: validaPotencialMapa [] ~=? False
     , "Tarefa 1 - Teste Valida Mapa com 2 portas" ~: validaPotencialMapa [(Porta, (0,0)), (Porta, (1,0))] ~=?  False
+    , "Tarefa 1 - Teste Valida Mapa sem portas" ~: validaPotencialMapa [(Bloco, (0,1)),(Bloco,(1,1))] ~=? False
+    , "Tarefa 1 - Teste Valida Mapa com caixas a flutuar" ~: validaPotencialMapa [(Caixa,(0,0)),(Caixa,(1,0)),(Porta,(1,1)),(Bloco,(0,2)),(Bloco,(1,2))] ~=? False
     ]

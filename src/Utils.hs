ce3000efb75df69f1bd7743833c2015e68c30587
Rui Martins <a100643@alunos.uminho.pt>
2021-11-20 23:10:12 +0000

    Add more functions to Utils.hs

--- src/Utils.hs
+++ src/Utils.hs
@@ -1,6 +1,7 @@
 module Utils where
 
 import LI12122
+import Data.List (sort)
 
 -- | Calcula a altura de um mapa.
 alturaMapa :: [(Peca, Coordenadas)] -> Int
@@ -17,3 +18,24 @@ larguraMapa [(f,(x,_))] = x + 1 -- ^ Caso exista apenas uma peça declarada.
 larguraMapa (p@(f,(x,y)):p'@(f',(x',y')):t)
     | x >= x' = larguraMapa (p:t)
     | otherwise = larguraMapa (p':t)
+
+-- | Encontra todas as peças declaradas numa linha.
+pecasNaLinha :: Int -> [(Peca, Coordenadas)] -> [(Peca, Coordenadas)]
+pecasNaLinha _ [] = []
+pecasNaLinha x (p@(f,(_,y)):t)
+    | x == y = p : pecasNaLinha x t
+    | otherwise = pecasNaLinha x t
+
+-- | Cria uma lista com todas as peças de um mapa.
+listaPecas :: [(Peca, Coordenadas)] -> [Peca]
+listaPecas l = map fst l
+-- | Cria uma lista de pares com todas as coordenadas de um mapa.
+
+sortedCoordenadas :: [(Peca, Coordenadas)] -> [Coordenadas]
+sortedCoordenadas l = sort (map snd l)
+
+-- | Junta duas listas num par - uma de peças e outra de coordenadas.
+juntaListas :: [Peca] -> [Coordenadas] -> [(Peca, Coordenadas)]
+juntaListas [] _ = []
+juntaListas _ [] = []
+juntaListas (h:t) (h':t') = (h,h') : juntaListas t t'

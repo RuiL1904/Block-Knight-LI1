25c7ace17e0fd349c0b0d031acd87410410bd41f
Rui Martins <a100643@alunos.uminho.pt>
2021-11-25 17:30:28 +0000

    Add show instance only for mapa type

--- src/Tarefa3_2021li1g033.hs
+++ src/Tarefa3_2021li1g033.hs
@@ -11,4 +11,26 @@ module Tarefa3_2021li1g033 where
 import LI12122
 
 instance Show Jogo where
-  show = undefined
+  show j = printJogo j
+
+printJogo :: Jogo -> String
+printJogo (Jogo m j) = mapaParaString m
+
+-- | Transforma um mapa numa string.
+mapaParaString :: Mapa -> String
+mapaParaString [] = ""
+mapaParaString (h:t) = linhaParaString h ++ "\n" ++ mapaParaString t
+
+-- | Auxiliar de mapaParaString - Transforma uma linha numa string.
+linhaParaString :: [Peca] -> String
+linhaParaString [] = ""
+linhaParaString (h:t) = pecaParaChar h : linhaParaString t
+
+-- | Auxiliar de linhaParaString - Transforma cada uma das peças no caractér correspondente.
+pecaParaChar :: Peca -> Char
+pecaParaChar p =
+  case p of 
+    Vazio -> ' '
+    Bloco -> 'X'
+    Caixa -> 'C'
+    Porta -> 'P'

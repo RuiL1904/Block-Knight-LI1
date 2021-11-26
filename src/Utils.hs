02f8982509229d9912f9aa00e6021f225277aed7
Rui Martins <a100643@alunos.uminho.pt>
2021-11-26 23:48:19 +0000

    Minor changes

--- src/Utils.hs
+++ src/Utils.hs
@@ -7,7 +7,7 @@ import Data.Function
 -- | Calcula a altura de um mapa.
 alturaMapa :: [(Peca, Coordenadas)] -> Int
 alturaMapa [] = 0
-alturaMapa [(f,(_,y))] = y + 1 -- ^ Caso exista apenas uma peça declarada.
+alturaMapa [(f,(_,y))] = y + 1 -- Caso exista apenas uma peça declarada.
 alturaMapa (p@(f,(x,y)):p'@(f',(x',y')):t)
     | y >= y' = alturaMapa (p:t)
     | otherwise = alturaMapa (p':t)
@@ -15,7 +15,7 @@ alturaMapa (p@(f,(x,y)):p'@(f',(x',y')):t)
 -- | Calcula a largura de um mapa.
 larguraMapa :: [(Peca, Coordenadas)] -> Int
 larguraMapa [] = 0
-larguraMapa [(f,(x,_))] = x + 1 -- ^ Caso exista apenas uma peça declarada.
+larguraMapa [(f,(x,_))] = x + 1 -- Caso exista apenas uma peça declarada.
 larguraMapa (p@(f,(x,y)):p'@(f',(x',y')):t)
     | x >= x' = larguraMapa (p:t)
     | otherwise = larguraMapa (p':t)

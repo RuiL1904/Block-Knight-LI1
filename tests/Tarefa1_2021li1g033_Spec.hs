65b89b7bde507c926451cfdd8541d9502c2105f5
Rui Martins <a100643@alunos.uminho.pt>
2021-11-27 22:36:14 +0000

    Add some tests

--- tests/Tarefa1_2021li1g033_Spec.hs
+++ tests/Tarefa1_2021li1g033_Spec.hs
@@ -5,7 +5,6 @@ import LI12122
 import Tarefa1_2021li1g033
 import Fixtures
 
--- Tarefa 1
 testsT1 =
   test
     [ "Tarefa 1 - Teste Valida Mapa m1" ~: validaPotencialMapa m1 ~=? True
@@ -13,4 +12,14 @@ testsT1 =
     , "Tarefa 1 - Teste Valida Mapa com 2 portas" ~: validaPotencialMapa [(Porta, (0,0)), (Porta, (1,0))] ~=?  False
     , "Tarefa 1 - Teste Valida Mapa sem portas" ~: validaPotencialMapa [(Bloco, (0,1)),(Bloco,(1,1))] ~=? False
     , "Tarefa 1 - Teste Valida Mapa com caixas a flutuar" ~: validaPotencialMapa [(Caixa,(0,0)),(Caixa,(1,0)),(Porta,(1,1)),(Bloco,(0,2)),(Bloco,(1,2))] ~=? False
+    , "Tarefa 1 - Teste Valida Chao m2" ~: validaChao m2 ~=? True 
+    , "Tarefa 1 - Teste Valida Chao m3" ~: validaChao m3 ~=? True
+    , "Tarefa 1 - Teste Valida Chao m4" ~: validaChao m4 ~=? False
+    , "Tarefa 1 - Teste Valida Chao m5" ~: validaChao m5 ~=? True
+    , "Tarefa 1 - Teste Valida Chao m6" ~: validaChao m6 ~=? False 
+    , "Tarefa 1 - Teste Valida Chao m7" ~: validaChao m7 ~=? False
+    , "Tarefa 1 - Testa Valida Mapa m8" ~: validaPotencialMapa m8 ~=? True
+    , "Tarefa 1 - Testa Valida Mapa m9" ~: validaPotencialMapa m9 ~=? True
+    , "Tarefa 1 - Teste Valida Mapa vazio" ~: validaPotencialMapa [] ~=? False
+    , "Tarefa 1 - Teste Valida Mapa com 2 portas" ~: validaPotencialMapa [(Porta, (0,0)), (Porta, (1,0))] ~=?  False
     ]

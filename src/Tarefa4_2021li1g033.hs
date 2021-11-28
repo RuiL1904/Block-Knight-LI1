8a0ad9b3e6180a149c2124304ec0b9470e541090
Digaz <a100646@alunos.uminho.pt>
2021-11-28 22:52:59 +0000

    Final Version for Tarefa4

--- src/Tarefa4_2021li1g033.hs
+++ src/Tarefa4_2021li1g033.hs
@@ -9,85 +9,81 @@ Módulo para a realização da Tarefa 4 do projeto de LI1 em 2021/22.
 module Tarefa4_2021li1g033 where
 
 import LI12122
-
+import Utils
 import Tarefa3_2021li1g033
 
-import Tarefa2_2021li1g033
-
-import Fixtures
-
-import Utils
-
 moveJogador :: Jogo -> Movimento -> Jogo
-moveJogador jogo = jogo 
-moveJogador jogo movimento = verificaMovimento jogo movimento 
+moveJogador j mov =
+    case mov of
+        AndarEsquerda -> moveEsquerda j
+        AndarDireita -> moveDireita j
+        Trepar -> podeTrepar j
+        InterageCaixa -> interageCaixa j
 
---Função auxiliar de verificaMovimento de moveJogador que indica qual a peça numa determinada coordenada
+correrMovimentos :: Jogo -> [Movimento] -> Jogo
+correrMovimentos j [] = j
+correrMovimentos j (h:t) = correrMovimentos (moveJogador j h) t
 
-encontraPeca :: Mapa -> Coordenadas -> Maybe Peca
-encontraPeca [] _ = Nothing
-encontraPeca (h:t) (x,0) = encontraNaColuna h x
-encontraPeca (h:t) (x,y) = encontraPeca t (x,y - 1)
+moveEsquerda :: Jogo -> Jogo
+moveEsquerda j =
+    case j of
+        Jogo m (Jogador (x,y) _ True)
+            | (pecaEsperada Vazio (x - 1,y) m || pecaEsperada Porta (x - 1,y) m) && (pecaEsperada Vazio (x - 1,y - 1) m || pecaImpossivel (x - 1,y - 1) m) -> Jogo m (Jogador (encontraBaixo (x - 1,y) m) Oeste True)
+            | otherwise -> Jogo m (Jogador (x,y) Oeste True)
+        Jogo m (Jogador (x,y) _ False)
+            | pecaEsperada Vazio (x - 1,y) m || pecaEsperada Porta (x - 1,y) m -> Jogo m (Jogador (encontraBaixo (x - 1,y) m) Oeste False)
+            | otherwise -> Jogo m (Jogador (x,y) Oeste False)
 
-encontraNaColuna :: [Peca] -> Int -> Maybe Peca
-encontraNaColuna [] _ = Nothing
-encontraNaColuna (h:t) 0 = Just h
-encontraNaColuna (h:t) x = encontraNaColuna t (x-1)
+moveDireita :: Jogo -> Jogo
+moveDireita j =
+    case j of
+        Jogo m (Jogador (x,y) _ True)
+            | (pecaEsperada Vazio (x + 1,y) m || pecaEsperada Porta (x - 1,y) m) && (pecaEsperada Vazio (x + 1,y - 1) m || pecaImpossivel (x + 1,y - 1) m) -> Jogo m (Jogador (encontraBaixo (x + 1,y) m) Este True)
+            | otherwise -> Jogo m (Jogador (x,y) Este True)
+        Jogo m (Jogador (x,y) _ False)
+            | pecaEsperada Vazio (x + 1,y) m || pecaEsperada Porta (x - 1,y) m -> Jogo m (Jogador (encontraBaixo (x + 1,y) m) Este False)
+            | otherwise -> Jogo m (Jogador (x,y) Este False)
 
---Função auxiliar de verificaMovimento que verifica se o movimento é possível
+podeTrepar :: Jogo -> Jogo
+podeTrepar j =
+    case j of
+        Jogo m (Jogador (x,y) Oeste True)
+            | pecaEsperada Bloco (x - 1,y) m || pecaEsperada Caixa (x - 1,y) m -> 
+                case (pecaEsperada Vazio (x - 1,y - 1) m || pecaEsperada Porta (x - 1,y - 1) m) && (pecaEsperada Vazio (x - 1,y - 2) m || pecaImpossivel (x - 1,y - 2) m) of
+                    True -> Jogo m (Jogador (x - 1,y - 1) Este True)
+                    _ -> j
+            | otherwise -> j
+        Jogo m (Jogador (x,y) Oeste False)
+            | pecaEsperada Bloco (x - 1,y) m || pecaEsperada Caixa (x - 1,y) m ->
+                case pecaEsperada Vazio (x - 1,y - 1) m || pecaEsperada Porta (x - 1,y - 1) m of
+                    True -> Jogo m (Jogador (x - 1,y - 1) Este False)
+                    _ -> j            
+            | otherwise -> j
+        Jogo m (Jogador (x,y) Este True)
+            | pecaEsperada Bloco (x + 1,y) m || pecaEsperada Caixa (x + 1,y) m ->
+                case (pecaEsperada Vazio (x + 1,y - 1) m || pecaEsperada Porta (x + 1,y - 1) m) && (pecaEsperada Vazio (x + 1,y - 2) m || pecaImpossivel (x + 1,y - 2) m) of
+                    True -> Jogo m (Jogador (x + 1,y - 1) Oeste True)
+                    _ -> j
+            | otherwise -> j
+        Jogo m (Jogador (x,y) Este False)
+            | pecaEsperada Bloco (x + 1,y) m || pecaEsperada Caixa (x + 1,y) m ->
+                case pecaEsperada Vazio (x + 1,y - 1) m || pecaEsperada Porta (x + 1,y - 1) m of
+                    True -> Jogo m (Jogador (x + 1,y - 1) Oeste False)
+                    _ -> j
+            | otherwise -> j
 
-verificaMovimento :: Jogo -> Movimento -> Jogo
-verificaMovimento (Jogo l (Jogador (x,y) d b)) m 
-    | d == Oeste && m == AndarDireita = Jogo l (Jogador (x,y) Este b)
-    | d == Oeste && m == AndarDireita && encontraPeca l (x, y + 1) == Just Vazio = error "Jogador não consegue estar nessa posição"
-    | d == Oeste && m == AndarEsquerda && encontraPeca l (x, y + 1) == Just Vazio = error "Jogador não consegue estar nessa posição"
-    | d == Oeste && m == AndarEsquerda && encontraPeca l (x - 1, y + 1) == Just Vazio = Jogo l (Jogador (x,y) d b) 
-    | d == Oeste && m == AndarEsquerda && encontraPeca l (x - 1,y) == Just Porta = Jogo l (Jogador (x,y) d b) 
-    | d == Oeste && m == AndarEsquerda && b == True && encontraPeca l (x, y - 1) == Just Bloco || encontraPeca l (x, y - 1) == Just Caixa = Jogo l (Jogador (x,y) d b)
-    | d == Oeste && m == AndarEsquerda && b == False && encontraPeca l (x - 1, y) == Just Bloco || encontraPeca l (x - 1, y) == Just Caixa = Jogo l (Jogador (x - 1,y) d b) 
-    | d == Oeste && m == AndarEsquerda && b == True && encontraPeca l (x - 1, y - 2) == Just Bloco || encontraPeca l (x - 1, y - 2) == Just Caixa  = Jogo l (Jogador (x,y) d b) 
-    | d == Oeste && m == AndarEsquerda && encontraPeca l (x - 1, y + 1) == Just Vazio = Jogo l (Jogador (x - 1, (maiorY (listaColuna (desconstroiMapa l) (x - 1))) - 1) d b)
-    | d == Oeste && m == Trepar && b == False && encontraPeca l (x - 1,y) == Just Bloco || encontraPeca l (x - 1, y + 1) == Just Caixa  && encontraPeca l (x - 1, y - 1) == Just Vazio = Jogo l (Jogador (x - 1, y - 1) d b)
-    | d == Oeste && m == Trepar && b == False && encontraPeca l (x - 1, y - 1) == Just Bloco || encontraPeca l (x - 1, y + 1) == Just Caixa  = Jogo l (Jogador (x,y) d b)
-    | d == Oeste && m == Trepar && b == True && encontraPeca l (x - 1, y - 2) == Just Bloco || encontraPeca l (x - 1, y - 2) == Just Caixa  = Jogo l (Jogador (x,y) d b)  
-    | d == Oeste && m == Trepar && b == True && encontraPeca l (x - 1, y) == Just Bloco || encontraPeca l (x - 1,y) == Just Caixa  && encontraPeca l (x - 1, y - 1) == Just Vazio && encontraPeca l (x - 1, y - 2) == Just Vazio = Jogo l (Jogador (x - 1, y - 1) d b)
-    | d == Oeste && m == InterageCaixa && b == False && encontraPeca l (x - 1, y) == Just Caixa && encontraPeca l (x, y - 1) == Just Bloco || encontraPeca l (x, y - 1) == Just Caixa  = Jogo l (Jogador (x,y) d b)
-    | d == Oeste && m == InterageCaixa && b == False && encontraPeca l (x - 1,y) == Just Caixa && encontraPeca l (x, y - 1) == Just Vazio = Jogo l (Jogador (x,y) d True)
-    | d == Este && m == AndarEsquerda = Jogo l (Jogador (x,y) Oeste b)
-    | d == Este && m == AndarEsquerda && encontraPeca l (x, y + 1) == Just Vazio = error "Jogador não consegue estar nessa posição"
-    | d == Este && m == AndarDireita && encontraPeca l (x, y + 1) == Just Vazio = error "Jogador não consegue estar nessa posição"
-    | d == Este && m == AndarDireita && encontraPeca l (x + 1, y - 1) == Just Vazio = Jogo l (Jogador (x,y) d b)
-    | d == Este && m == AndarDireita && encontraPeca l (x + 1,y) == Just Porta = Jogo l (Jogador (x,y) d b)
-    | d == Este && m == AndarDireita && b == True && encontraPeca l (x, y - 1) == Just Bloco || encontraPeca l (x, y - 1) == Just Caixa = error "Jogador não consegue estar nessa posição com a caixa"
-    | d == Este && m == AndarDireita && b == False && encontraPeca l (x + 1, y + 1) == Just Bloco || encontraPeca l (x - 1, y + 1) == Just Caixa  && encontraPeca l (x - 1,y) == Just Vazio = Jogo l (Jogador (x + 1,y) d b) 
-    | d == Este && m == AndarDireita && b == True && encontraPeca l (x + 1, y - 2) == Just Bloco || encontraPeca l (x - 1, y + 1) == Just Caixa  = Jogo l (Jogador (x,y) d b) 
-    | d == Este && m == AndarDireita && encontraPeca l (x + 1, y + 1) == Just Vazio = Jogo l (Jogador (x + 1, (maiorY (listaColuna (desconstroiMapa l) (x - 1))) - 1) d b)
-    | d == Este && m == Trepar && b == False && encontraPeca l (x + 1,y) == Just Bloco || encontraPeca l (x - 1, y + 1) == Just Caixa  && encontraPeca l (x + 1, y - 1) == Just Vazio = Jogo l (Jogador (x + 1, y - 1) d b)
-    | d == Este && m == Trepar && b == False && encontraPeca l (x + 1, y - 1) == Just Bloco || encontraPeca l (x - 1, y + 1) == Just Caixa  = Jogo l (Jogador (x,y) d b)
-    | d == Este && m == Trepar && b == True && encontraPeca l (x + 1, y - 2) == Just Bloco || encontraPeca l (x - 1, y - 2) == Just Caixa  = Jogo l (Jogador (x,y) d b)  
-    | d == Este && m == Trepar && b == True && encontraPeca l (x + 1,y) == Just Bloco || encontraPeca l (x + 1,y) == Just Caixa  && encontraPeca l (x + 1, y - 1) == Just Vazio && encontraPeca l (x + 1, y - 2) == Just Vazio = Jogo l (Jogador (x + 1, y - 1) d b)
-    | d == Este && m == InterageCaixa && b == False&& encontraPeca l (x + 1, y) == Just Caixa && encontraPeca l (x, y - 1) == Just Bloco || encontraPeca l (x, y - 1) == Just Caixa  = Jogo l (Jogador (x,y) d b)
-    | d == Este && m == InterageCaixa && b == False && encontraPeca l (x + 1,y) == Just Caixa && encontraPeca l (x, y - 1) == Just Vazio = Jogo l (Jogador (x,y) d True)    
-
---Função auxiliar de verificaMovimento que introduzindo o x da coluna define uma lista com os elementos dessa coluna  
-
-maiorY :: [(Peca, Coordenadas)] -> Int
-maiorY [] = (-1)
-maiorY l = maiorYCoord (listaCoordenadas l) 
-
---Função auxiliar de verificaMovimento que calcula o maior y de uma lista de coordenadas    
-
-maiorYCoord :: [Coordenadas] -> Int 
-maiorYCoord [] = error "Não existe"
-maiorYCoord [(_,y)] = y
-maiorYCoord ((x,y):(xs,ys):t) 
-    | y >= ys = maiorYCoord ((x,y):t)
-    | otherwise = maiorYCoord ((xs,ys):t)
-
---Função auxiliar para verificaMovimento - dá uma lista das coordenadas de peças de uma determinada coluna    
-
-listaColuna :: [(Peca, Coordenadas)] -> Int -> [(Peca, Coordenadas)]
-listaColuna [] n = []
-listaColuna ((h, (x,y)):t) n 
-    | x == n = (h, (x,y)) : listaColuna t n
-    | otherwise = listaColuna t n 
+interageCaixa :: Jogo -> Jogo
+interageCaixa j =
+    case j of
+        Jogo m (Jogador (x,y) Oeste True)
+            | pecaEsperada Vazio (x - 1,y - 1) m -> Jogo (substituiPeca (encontraBaixo (x - 1,y) m) Caixa m) (Jogador (x,y) Oeste False)
+            | otherwise -> j
+        Jogo m (Jogador (x,y) Oeste False)
+            | pecaEsperada Caixa (x - 1,y) m && pecaEsperada Vazio (x - 1,y - 1) m && pecaEsperada Vazio (x,y - 1) m -> Jogo (substituiPeca (x - 1,y) Vazio m) (Jogador (x,y) Oeste True)
+            | otherwise -> j
+        Jogo m (Jogador (x,y) Este True)
+            | pecaEsperada Vazio (x + 1,y - 1) m -> Jogo (substituiPeca (encontraBaixo (x + 1,y) m) Caixa m) (Jogador (x,y) Este False)
+            | otherwise -> j
+        Jogo m (Jogador (x,y) Este False)
+            | pecaEsperada Caixa (x + 1,y) m && pecaEsperada Vazio (x + 1,y - 1) m && pecaEsperada Vazio (x,y - 1) m -> Jogo (substituiPeca (x + 1,y) Vazio m) (Jogador (x,y) Este True)
+            | otherwise -> j

a94600fe7b021004f2c782d423456b3e77f7a1b0
Rui Martins <a100643@alunos.uminho.pt>
2021-12-18 18:18:31 +0000

    Add Tarefa 6 boilerplate

---
+++ src/Tarefa6_2021li1g033.hs
@@ -0,0 +1,17 @@
+{- |
+Module      : Tarefa6_2021li1g033
+Description : Resolução de um puzzle
+
+Módulo para a realização da Tarefa 6 do projeto de LI1 em 2021/22.
+-}
+module Tarefa6_2021li1g033 where
+
+import LI12122
+import Tarefa1_2021li1g033
+import Tarefa2_2021li1g033
+import Tarefa3_2021li1g033
+import Tarefa4_2021li1g033
+import Utils
+
+resolveJogo :: Int -> Jogo -> Maybe [Movimento]
+resolveJogo i jogo = undefined

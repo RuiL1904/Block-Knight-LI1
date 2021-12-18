bb7942f7b2c8dbb2ee2fd53e625040b2ca6859ba
Rui Martins <a100643@alunos.uminho.pt>
2021-12-18 18:18:13 +0000

    Add Tarefa 5 boilerplate

---
+++ src/Tarefa5_2021li1g033.hs
@@ -0,0 +1,23 @@
+{- |
+Module      : Tarefa5_2021li1g033
+Description : Aplicação Gráfica
+
+Módulo para a realização da Tarefa 5 do projeto de LI1 em 2021/22.
+-}
+module Main where
+
+import LI12122
+import Tarefa1_2021li1g033
+import Tarefa2_2021li1g033
+import Tarefa3_2021li1g033
+import Tarefa4_2021li1g033
+import Utils
+
+import Graphics.Gloss
+import Graphics.Gloss.Interface.IO.Game
+import Graphics.Gloss.Juicy (loadJuicy)
+import System.Exit
+
+main :: IO ()
+main = do
+    undefined

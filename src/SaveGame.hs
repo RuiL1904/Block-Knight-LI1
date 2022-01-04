560604476b85bfbbc52a83c7ab328972ca779260
Rui Martins <a100643@alunos.uminho.pt>
2022-01-04 23:08:09 +0000

    Implement save game feature

---
+++ src/SaveGame.hs
@@ -0,0 +1,42 @@
+{-# LANGUAGE DeriveGeneric, DeriveAnyClass #-}
+module SaveGame where
+
+import LI12122
+import Tarefa3_2021li1g033
+import Niveis
+
+import GHC.Generics
+import Control.DeepSeq
+import System.IO
+import qualified System.IO.Strict as SIO
+
+data JogoGuardado = 
+    JogoGuardado
+        Mapa
+        Jogador 
+    deriving (Show, Read, Generic, NFData)
+
+lerSGame :: IO (JogoGuardado)
+lerSGame = SIO.run $ lerSGame' "data/saveGame.txt"
+
+lerSGame' :: String -> SIO.SIO (JogoGuardado)
+lerSGame' fn = do
+    handle <- SIO.openFile fn ReadMode
+    c <- SIO.hGetContents handle
+    SIO.hClose handle
+    SIO.return' ((read c) :: JogoGuardado)
+
+escreverSGame :: JogoGuardado -> IO ()
+escreverSGame j = SIO.run $ escreverSGame' "data/saveGame.txt" (show j)
+
+escreverSGame' :: String -> String -> SIO.SIO ()
+escreverSGame' fn s = do
+    handle <- SIO.openFile fn WriteMode
+    SIO.hPutStr handle s
+    SIO.hClose handle
+
+guardadoParaJogo :: JogoGuardado -> Jogo
+guardadoParaJogo (JogoGuardado m j) = (Jogo m j)
+
+jogoParaGuardado :: Jogo -> JogoGuardado
+jogoParaGuardado (Jogo m j) = (JogoGuardado m j)

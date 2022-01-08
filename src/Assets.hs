09e409d78d41e1b34cce62e36fb998e615e662eb
Rui Martins <a100643@alunos.uminho.pt>
2022-01-08 23:58:15 +0000

    Update Imagens Record

--- src/Assets.hs
+++ src/Assets.hs
@@ -1,13 +1,20 @@
-module Assets where
+module Src.Assets where
 
 import Graphics.Gloss
 import Graphics.Gloss.Juicy (loadJuicyPNG, loadJuicyJPG)
 
 -- | O conjunto das imagens inerentes ao programa.
 data Imagens = Imagens {
-    background :: Picture,
+    background1 :: Picture,
+    background2 :: Picture,
+    background3 :: Picture,
+    background4 :: Picture,
     mpJogar :: Picture,
     mpOpcoes :: Picture,
+    eBackground1 :: Picture,
+    eBackground2 :: Picture,
+    eBackground3 :: Picture,
+    eBackground4 :: Picture,
     mpCreditos :: Picture,
     mpSair :: Picture,
     creditos :: Picture,
@@ -30,17 +37,25 @@ data Imagens = Imagens {
     caixa :: Picture,
     porta :: Picture,
     knightLeft :: Picture,
-    knightRight :: Picture
+    knightRight :: Picture,
+    victory :: Picture
     -- Adicionar as imagens necessárias aqui no futuro.
 }
 
 -- Carrega todas as imagens para o data type 'Imagens'.
 carregaImagens :: IO Imagens
 carregaImagens = do
-    Just background <- loadJuicyJPG "assets/background.jpg"
+    Just background1 <- loadJuicyJPG "assets/background1.jpg"
+    Just background2 <- loadJuicyJPG "assets/background2.jpg"
+    Just background3 <- loadJuicyJPG "assets/background3.jpg"
+    Just background4 <- loadJuicyJPG "assets/background4.jpg"
     -- MenuPrincipal.
     Just mpJogar <- loadJuicyJPG "assets/mpJogar.jpg"
     Just mpOpcoes <- loadJuicyJPG "assets/mpOpcoes.jpg"
+    Just eBackground1 <- loadJuicyJPG "assets/eBackground1.jpg"
+    Just eBackground2 <- loadJuicyJPG "assets/eBackground2.jpg"
+    Just eBackground3 <- loadJuicyJPG "assets/eBackground3.jpg"
+    Just eBackground4 <- loadJuicyJPG "assets/eBackground4.jpg"
     Just mpCreditos <- loadJuicyJPG "assets/mpCreditos.jpg"
     Just mpSair <- loadJuicyJPG "assets/mpSair.jpg"
     -- Opções do MenuPrincipal.
@@ -67,5 +82,6 @@ carregaImagens = do
     Just porta <- loadJuicyPNG "assets/porta.png"
     Just knightLeft <- loadJuicyPNG "assets/knightLeft.png"
     Just knightRight <- loadJuicyPNG "assets/knightRight.png"
+    Just victory <- loadJuicyJPG "assets/victory.jpg"
     
-    return (Imagens background mpJogar mpOpcoes mpCreditos mpSair creditos mjEscolhe mjArcade mjCarregar mj1 mj2 mj3 mj4 mj5 mj6 mj7 mj8 mj9 mj10 setaBaixo setaCima bloco caixa porta knightLeft knightRight)
+    return (Imagens background1 background2 background3 background4 mpJogar mpOpcoes eBackground1 eBackground2 eBackground3 eBackground4 mpCreditos mpSair creditos mjEscolhe mjArcade mjCarregar mj1 mj2 mj3 mj4 mj5 mj6 mj7 mj8 mj9 mj10 setaBaixo setaCima bloco caixa porta knightLeft knightRight victory)
